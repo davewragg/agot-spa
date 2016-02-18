db07cd3d15edb0c49fbbc9e440c2e06a82c88d90
David Wragg <David.Wragg@rufusleonard.com>
2016-02-18 14:40:28 +0000

    Namespace app selector

--- src/app/components/app.ts
+++ src/app/components/app.ts
@@ -1,15 +1,12 @@
 import {Component, ViewEncapsulation} from 'angular2/core';
-import {
-  RouteConfig,
-  ROUTER_DIRECTIVES
-} from 'angular2/router';
+import {RouteConfig, ROUTER_DIRECTIVES} from 'angular2/router';
 
 import {HomeCmp} from '../../home/components/home';
 import {AboutCmp} from '../../about/components/about';
 import {NameList} from '../../shared/services/name_list';
 
 @Component({
-  selector: 'app',
+  selector: 'agot-app',
   viewProviders: [NameList],
   moduleId: module.id,
   templateUrl: './app.html',
@@ -18,8 +15,10 @@ import {NameList} from '../../shared/services/name_list';
   directives: [ROUTER_DIRECTIVES]
 })
 @RouteConfig([
-  {path: '/', component: HomeCmp, name: 'Home'},
-  {path: '/about', component: AboutCmp, name: 'About'}
+  {path: '/', component: HomeCmp, name: 'Home', useAsDefault: true},
+  {path: '/about', component: AboutCmp, name: 'About'},
+  //{path: '/decks', component: DecksCmp, name: 'Decks'},
+  //{path: '/players', component: PlayersCmp, name: 'Players'},
 ])
 export class AppCmp {
 }
