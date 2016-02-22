3a9095bb37ae98dc8b5bf5797847ec2b4d666da9
Dave <david.wragg@gmail.com>
2016-02-22 12:04:34 +0000

    Cache the data response

--- src/shared/services/data.service.ts
+++ src/shared/services/data.service.ts
@@ -6,19 +6,25 @@ import {GameIndex} from '../models/game-index.model';
 
 @Injectable()
 export class DataService {
+  private data: Observable<GameIndex>;
+
   constructor(private http:Http) {
   }
 
   getGameIndex():Observable<GameIndex> {
+    if (!this.data) {
+      this.data = this._getGameIndex();
+    }
+    return this.data;
+  }
+
+  private _getGameIndex():Observable<GameIndex> {
     return this.getFromJson()
       .map((res:Response) => res.json());
   }
 
   private getFromJson():Observable<Response> {
-    return this.http.get('/assets/data/GetAll.json', {
-      // TODO find out how this works
-      //cache: true
-    });
+    return this.http.get('/assets/data/GetAll.json');
   };
 
   //private getFromWeb():Observable<Response> {
