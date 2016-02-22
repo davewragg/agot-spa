fed0ad59435962f3c1293b368abb15bed4c67413
Dave <david.wragg@gmail.com>
2016-02-22 11:25:18 +0000

    Implement player rankings and form table

--- src/home/components/rankings.cmp.ts
+++ src/home/components/rankings.cmp.ts
@@ -5,32 +5,9 @@ import {SetOfResults} from '../../shared/models/set-of-results.model';
 @Component({
   selector: 'agot-rankings',
   moduleId: module.id,
-  //templateUrl: './rankings.cmp.html',
+  templateUrl: './rankings.cmp.html',
   styleUrls: ['./rankings.cmp.css'],
-  template: `
-    <section *ngIf="rankings">
-      <h3>{{ name }} Rankings</h3>
-      <div class="ranking-category">
-        <h4>Player ranking</h4>
-        <ol>
-          <li *ngFor="#ranking of rankings.rankedPlayers">{{ ranking.playerName }}</li>
-        </ol>
-      </div>
-      <div class="ranking-category">
-        <h4>Faction ranking</h4>
-        <ol>
-          <li *ngFor="#ranking of rankings.rankedFactions">{{ ranking.playerName }}</li>
-        </ol>
-      </div>
-      <div class="ranking-category">
-        <h4>Agenda ranking</h4>
-        <ol>
-          <li *ngFor="#ranking of rankings.rankedAgendas">{{ ranking.playerName }}</li>
-        </ol>
-      </div>
-    </section>
-  `,
-  pipes: []
+  directives: []
 })
 export class RankingsCmp {
   @Input()
---
+++ src/shared/models/game-result.model.ts
@@ -0,0 +1,5 @@
+export enum GameResult {
+  Win,
+  Lose,
+  Draw
+}
