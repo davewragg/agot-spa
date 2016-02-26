082e9baa74e9716f7a025f7d1fff339de00debe4
Dave <david.wragg@gmail.com>
2016-02-26 11:34:55 +0000

    Add reverse pipe to reverse order of form table

--- src/home/components/rankings.component.ts
+++ src/home/components/rankings.component.ts
@@ -1,13 +1,14 @@
 import {Input, Component} from 'angular2/core';
 
 import {SetOfResults} from '../../shared/models/set-of-results.model';
+import {ReversePipe} from '../../shared/pipes/reverse-pipe';
 
 @Component({
   selector: 'agot-rankings',
   moduleId: module.id,
   templateUrl: './rankings.html',
   styleUrls: ['./rankings.css'],
-  directives: []
+  pipes: [ReversePipe]
 })
 export class RankingsComponent {
   @Input()
