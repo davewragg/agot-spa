69a39fd2da06611a7187f4599c5385a540302c32
Dave <david.wragg@gmail.com>
2016-02-26 13:13:52 +0000

    Hook up the core game data to view component

--- src/game/view-game.component.ts
+++ src/game/view-game.component.ts
@@ -2,12 +2,14 @@ import {Component, Input, Output, EventEmitter} from 'angular2/core';
 import {ROUTER_DIRECTIVES} from 'angular2/router';
 import {Game} from '../shared/models/game.model';
 import {GamePlayersComponent} from './game-players.component';
+import {DateFormatPipe} from '../shared/pipes/date-format-pipe';
 
 @Component({
   selector: 'agot-view-game',
   moduleId: module.id,
   templateUrl: './view-game.html',
-  directives: [GamePlayersComponent, ROUTER_DIRECTIVES]
+  directives: [GamePlayersComponent, ROUTER_DIRECTIVES],
+  pipes: [DateFormatPipe]
 })
 export class ViewGameComponent {
   @Input()
