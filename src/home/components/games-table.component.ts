645b761d12adb3ec96330bafd4c18a78df0da3f3
Dave <david.wragg@gmail.com>
2016-02-28 15:18:04 +0000

    Agenda badge component

--- src/home/components/games-table.component.ts
+++ src/home/components/games-table.component.ts
@@ -4,13 +4,14 @@ import {ROUTER_DIRECTIVES} from 'angular2/router';
 import {Game} from '../../shared/models/game.model';
 import {DateFormatPipe} from '../../shared/pipes/date-format-pipe';
 import {FactionBadgeComponent} from '../../shared/components/faction-badge.component';
+import {AgendaBadgeComponent} from '../../shared/components/agenda-badge.component';
 
 @Component({
   selector: 'agot-games-table',
   moduleId: module.id,
   templateUrl: './games-table.html',
   pipes: [DateFormatPipe],
-  directives: [ROUTER_DIRECTIVES, FactionBadgeComponent]
+  directives: [ROUTER_DIRECTIVES, FactionBadgeComponent, AgendaBadgeComponent]
 })
 export class GamesTableComponent {
   @Input()
