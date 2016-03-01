eec5e497ff825ef2eb305db6c5f5a952c75aaf02
Dave <david.wragg@gmail.com>
2016-03-01 21:28:53 +0000

    Convert to reduce to build stats object

--- src/shared/services/player.service.ts
+++ src/shared/services/player.service.ts
@@ -27,41 +27,85 @@ export class PlayerService {
   }
 
   getPlayerStats(playerId:number):Observable<any> {
-    const stats = {
+    const stats = <PlayerStats>{
       games: [],
-      factions: new Map(),
-      agendas: new Map(),
-      players: new Map()
+      overall: <Stats>{won: 0, drawn: 0, lost: 0},
+      factionsVs: new Map<number, Stats>(),
+      factionsAs: new Map<number, Stats>(),
+      agendasVs: new Map<number, Stats>(),
+      agendasAs: new Map<number, Stats>(),
+      playersVs: new Map<number, Stats>()
     };
     return this._dataService.getGameIndex().map((gameIndex:GameIndex) => {
       return gameIndex.allResults.games.filter((game:Game) => {
         return !!game.gamePlayers.find(
           (gamePlayer:GamePlayer) => gamePlayer.playerId === playerId
         );
-      }).forEach((game:Game) => {
+      }).reduce((lastGame:Game, game:Game, currentIndex:number, games:Game[]):any => {
         stats.games.push(game);
+        const winner:GamePlayer = game.gamePlayers.find((gamePlayer:GamePlayer) => gamePlayer.isWinner);
         const me = game.gamePlayers.find((gamePlayer:GamePlayer) => gamePlayer.playerId === playerId);
-        const isWinner = me.isWinner;
+        const result:Result = me.isWinner ? Result.WON : !!winner ? Result.LOST : Result.DREW;
 
-        if (me.agendaId) {
-          const agendaStats = stats.agendas.get(me.agendaId) || <Stats>{won: 0, drawn: 0, lost: 0};
-          if (isWinner) {
-            agendaStats.won++;
-          } else {
-            agendaStats.lost++;
+        const overallStats = stats.overall;
+        this.setResult(overallStats, result);
+
+        this.setStats(me.agendaId, stats.agendasAs, result);
+        this.setStats(me.factionId, stats.factionsAs, result);
+        this.setStats(me.secondFactionId, stats.factionsAs, result);
+
+        game.gamePlayers.forEach((gamePlayer:GamePlayer) => {
+          if (gamePlayer.playerId !== me.playerId) {
+            this.setStats(gamePlayer.playerId, stats.playersVs, result);
+            this.setStats(gamePlayer.agendaId, stats.agendasVs, result);
+            this.setStats(gamePlayer.factionId, stats.factionsVs, result);
+            this.setStats(gamePlayer.secondFactionId, stats.factionsVs, result);
           }
-          stats.agendas.set(me.agendaId, agendaStats);
-        }
+        });
+
         return stats;
-      });
-    }).map(() => {
-      return stats;
+      }, stats);
     });
   }
+
+  private setStats(keyId, statsMap, result:Result) {
+    if (keyId) {
+      const keyStats = statsMap.get(keyId) || <Stats>{won: 0, drawn: 0, lost: 0};
+      this.setResult(keyStats, result);
+      statsMap.set(keyId, keyStats);
+    }
+  };
+
+  private setResult(keyStats, result) {
+    keyStats.played++;
+    if (result === Result.WON) {
+      keyStats.won++;
+    } else if (result === Result.DREW) {
+      keyStats.drawn++;
+    } else {
+      keyStats.lost++;
+    }
+  };
+}
+interface PlayerStats {
+  games: Game[];
+  overall: Stats;
+  factionsVs: Map<number, Stats>;
+  factionsAs: Map<number, Stats>;
+  agendasVs: Map<number, Stats>;
+  agendasAs: Map<number, Stats>;
+  playersVs: Map<number, Stats>;
 }
 
 interface Stats {
+  played:number;
   won:number;
   drawn:number;
   lost:number;
+}
+
+enum Result {
+  LOST = -1,
+  DREW = 0,
+  WON = 1,
 }
