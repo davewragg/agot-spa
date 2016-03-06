f4f92ed1507c301fc01909cdbbd7b8366e24da2c
Dave <david.wragg@gmail.com>
2016-03-06 17:40:00 +0000

    use some proper style bindings i suppose

--- src/shared/components/deck-class-block.component.ts
+++ src/shared/components/deck-class-block.component.ts
@@ -6,7 +6,7 @@ import {DeckClass} from '../models/deck-class.model';
   moduleId: module.id,
   template: `
     <div *ngIf="deckClass" class="text-xs-center deck-class-block"
-         style="background-image: url(/assets/img/agenda{{ deckClass.agenda?.agendaId }}.png)">
+    [style.backgroundImage]="deckClass.agenda?.agendaId && ('url(/assets/img/agenda' + deckClass.agenda?.agendaId + '.png)')">
       <div class="card-header {{ headerClass }}">{{ title }}</div>
       <img class="card-img" src="/assets/img/icon{{ deckClass.faction.factionId }}.png"
          alt="{{ deckClass.faction.factionName }}">
