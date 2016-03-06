92a09d7b63d771bd9b2bd91d9dc0f8e96dc4dc4e
Dave <david.wragg@gmail.com>
2016-03-06 15:58:27 +0000

    Layout and tarting up of deck class blocks

--- src/shared/components/deck-class-block.component.ts
+++ src/shared/components/deck-class-block.component.ts
@@ -5,20 +5,27 @@ import {DeckClass} from '../models/deck-class.model';
   selector: 'agot-deck-class-block',
   moduleId: module.id,
   template: `
-    <div class="text-xs-center">
-      <div class="card-header">{{ title }}</div>
+    <div class="text-xs-center deck-class-block"
+         style="background-image: url(/assets/img/icon{{ deckClass.agenda?.agendaId }}.png)">
+      <div class="card-header {{ headerClass }}">{{ title }}</div>
       <img class="card-img" src="/assets/img/icon{{ deckClass.faction.factionId }}.png"
          alt="{{ deckClass.faction.factionName }}">
       <div class="card-footer text-muted small">{{ deckClass.title }}</div>
     </div>
-    <!--<img *ngIf="deckClass.agenda" class="faction-badge" src="/assets/img/icon{{ deckClass.agenda.agendaId }}.png"-->
-         <!--alt="{{ deckClass.agenda.title }}">-->
   `,
-  styles: [``]
+  styles: [`
+    .deck-class-block {
+      background-repeat: no-repeat;
+      background-position: center center;
+      background-size: cover;
+    }
+  `]
 })
 export class DeckClassBlockComponent {
   @Input()
   title:string;
+  @Input()
+  headerClass:string;
   @Input()
   deckClass:DeckClass;
 }
