26a261b22cd3c426012af98c9ff80e2b84ba8804
Dave <david.wragg@gmail.com>
2016-03-07 21:54:37 +0000

    Toggle show games

--- src/players/player-stats.component.ts
+++ src/players/player-stats.component.ts
@@ -4,17 +4,20 @@ import {Player} from '../shared/models/player.model';
 import {CountComponent} from '../shared/components/count.component';
 import {PlayerStatsTableComponent} from './player-stats-table.component';
 import {PlayerInsightsComponent} from './player-insights.component';
+import {GamesTableComponent} from '../home/components/games-table.component';
 
 @Component({
   selector: 'agot-player-stats',
   moduleId: module.id,
   changeDetection: ChangeDetectionStrategy.OnPush,
   templateUrl: './player-stats.html',
-  directives: [CountComponent, PlayerStatsTableComponent, PlayerInsightsComponent]
+  directives: [CountComponent, PlayerStatsTableComponent, PlayerInsightsComponent, GamesTableComponent]
 })
 export class PlayerStatsComponent {
   @Input()
   player:Player;
   @Input()
   playerStats:PlayerStats;
+
+  showGames:boolean = false;
 }
