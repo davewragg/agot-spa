ce57fd7ebba7ef5df75e7219f8903752a3443017
Dave <david.wragg@gmail.com>
2016-03-10 11:00:21 +0000

    Latest updates from seed

--- tools/config/project.config.ts
+++ tools/config/project.config.ts
@@ -2,14 +2,14 @@ import {join} from 'path';
 import {SeedConfig, normalizeDependencies} from './seed.config';
 
 export class ProjectConfig extends SeedConfig {
-  PROJECT_TASKS_DIR    = join(process.cwd(), this.TOOLS_DIR, 'tasks', 'project');
+  PROJECT_TASKS_DIR = join(process.cwd(), this.TOOLS_DIR, 'tasks', 'project');
 
   constructor() {
     super();
-     this.APP_TITLE = 'AGOT Tracker';
-    let additional_deps = [
-      { src: 'bootstrap/dist/css/bootstrap.min.css', inject: true },
-      { src: 'angular2-toaster/lib/toaster.css', inject: true },
+    this.APP_TITLE = 'AGOT Tracker';
+    let additional_deps:Array<any> = [
+      {src: 'bootstrap/dist/css/bootstrap.min.css', inject: true, vendor: true},
+      {src: 'angular2-toaster/lib/toaster.css', inject: true, vendor: true},
       // {src: 'jquery/dist/jquery.min.js', inject: 'libs'},
       // {src: 'lodash/lodash.min.js', inject: 'libs'},
     ];
@@ -20,7 +20,7 @@ export class ProjectConfig extends SeedConfig {
     this.APP_ASSETS = [
       // {src: `${this.ASSETS_SRC}/css/toastr.min.css`, inject: true},
       // {src: `${this.APP_DEST}/assets/scss/global.css`, inject: true},
-      {src: `${this.ASSETS_SRC}/main.css`, inject: true},
+      {src: `${this.ASSETS_SRC}/main.css`, inject: true, vendor: false},
     ];
 
     this.DEV_DEPENDENCIES = this.DEV_NPM_DEPENDENCIES.concat(this.APP_ASSETS);
