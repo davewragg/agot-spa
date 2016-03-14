9d2c713478b32552205435121e6110b1a2fb4cb1
Dave <david.wragg@gmail.com>
2016-03-14 11:47:41 +0000

    Trim unused method

--- src/shared/services/game.service.ts
+++ src/shared/services/game.service.ts
@@ -2,18 +2,12 @@ import {Injectable} from 'angular2/core';
 import {Game} from '../models/game.model';
 import {Observable} from 'rxjs/Observable';
 import {DataService} from './data.service';
-import {GameIndex} from '../models/game-index.model';
 import {FilterCriteria} from '../models/filter-criteria.model';
 
 @Injectable()
 export class GameService {
 
   constructor(private dataService:DataService) {
-  }
-
-  getAllGames():Observable<Game[]> {
-    return this.dataService.getGameIndex()
-      .map((gameIndex:GameIndex) => gameIndex.allResults.games);
   }
 
   getGames(filterCriteria?:FilterCriteria):Observable<Game[]> {
