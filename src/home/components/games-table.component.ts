9e9807382db5cc1cb4812ad692e5afaedfe61d5b
Dave Wragg <david.wragg@gmail.com>
2016-03-24 21:50:33 +0000

    Add venue to games table

--- src/home/components/games-table.component.ts
+++ src/home/components/games-table.component.ts
@@ -1,12 +1,14 @@
 import {Component, Input, ChangeDetectionStrategy} from 'angular2/core';
 import {ROUTER_DIRECTIVES} from 'angular2/router';
-
 import {Game} from '../../shared/models/game.model';
 import {FactionBadgeComponent} from '../../shared/components/faction-badge.component';
 import {AgendaBadgeComponent} from '../../shared/components/agenda-badge.component';
 import {PlayerLinkComponent} from '../../shared/components/player-link.component';
 import {TimeAgoPipe} from '../../shared/pipes/time-ago-pipe';
 import {DateFormatPipe} from '../../shared/pipes/date-format-pipe';
+import {ReferenceDataService} from '../../shared/services/reference-data.service';
+import {Observable} from 'rxjs/Observable';
+import {Venue} from '../../shared/models/venue.model';
 
 @Component({
   selector: 'agot-games-table',
@@ -19,4 +21,17 @@ import {DateFormatPipe} from '../../shared/pipes/date-format-pipe';
 export class GamesTableComponent {
   @Input()
   games:Game[];
+
+  constructor(private _referenceDataService:ReferenceDataService) {
+  }
+
+  getVenue(venueId:number):Observable<Venue> {
+    return this._referenceDataService.getVenue(venueId);
+  }
+
+  getVenueName(venueId:number):Observable<string> {
+    return this.getVenue(venueId).map((venue:Venue) => {
+      return venue && venue.name;
+    });
+  }
 }
