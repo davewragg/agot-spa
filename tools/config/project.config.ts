18273e46b99739a60b22e88a1cfe95ee8d755d64
Dave Wragg <david.wragg@gmail.com>
2016-04-10 12:49:07 +0100

    Add scss support via https://github.com/mgechev/angular2-seed/wiki/Add-Sass-support

--- tools/config/project.config.ts
+++ tools/config/project.config.ts
@@ -25,11 +25,14 @@ export class ProjectConfig extends SeedConfig {
     this.APP_ASSETS = [
       // {src: `${this.ASSETS_SRC}/css/toastr.min.css`, inject: true},
       // {src: `${this.APP_DEST}/assets/scss/global.css`, inject: true},
-      { src: `${this.ASSETS_SRC}/main.css`, inject: true },
+      // { src: `${this.ASSETS_SRC}/main.css`, inject: true }, // the old css file
+      { src: `${this.ASSETS_SRC}/main.scss`, inject: true }, // renamed SASS file
     ];
     // Dev
     this.SYSTEM_CONFIG.paths['lodash'] = `${this.APP_BASE}node_modules/lodash/index`;
     // Prod
     this.SYSTEM_BUILDER_CONFIG.paths['lodash'] = `node_modules/lodash/index.js`;
+
+    this.CSS_PROD_BUNDLE = 'main.css';
   }
 }
