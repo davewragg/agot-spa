b3d45d727ab7f3db27fe15dcdc42913e74b1cf11
Dave Wragg <david.wragg@gmail.com>
2016-12-27 21:08:24 +0000

    Provide all services at app-level

--- src/client/app/app.module.ts
+++ src/client/app/app.module.ts
@@ -4,19 +4,45 @@ import { APP_BASE_HREF } from '@angular/common';
 import { HttpModule } from '@angular/http';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
-
 import { AboutModule } from './about/about.module';
 import { HomeModule } from './home/home.module';
 import { SharedModule } from './shared/shared.module';
+import {
+  CacheService,
+  DataService,
+  DeckService,
+  GameService,
+  NotificationService,
+  PlayerService,
+  RankingService,
+  ReferenceDataService,
+  SeasonService,
+  StatsService,
+  ThronesDbService
+} from './shared/services/index';
 
 @NgModule({
   imports: [BrowserModule, HttpModule, AppRoutingModule, AboutModule, HomeModule, SharedModule.forRoot()],
   declarations: [AppComponent],
-  providers: [{
-    provide: APP_BASE_HREF,
-    useValue: '<%= APP_BASE %>'
-  }],
+  providers: [
+    {
+      provide: APP_BASE_HREF,
+      useValue: '<%= APP_BASE %>'
+    },
+    CacheService,
+    DataService,
+    DeckService,
+    GameService,
+    NotificationService,
+    PlayerService,
+    RankingService,
+    ReferenceDataService,
+    SeasonService,
+    StatsService,
+    ThronesDbService
+  ],
   bootstrap: [AppComponent]
 
 })
-export class AppModule { }
+export class AppModule {
+}
