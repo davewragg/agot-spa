28b0d9df622c2d945014aefa5c46e5e167ee494b
Dave Wragg <david.wragg@gmail.com>
2016-12-29 16:01:56 +0000

    Fix criteria deserialisation (perhaps)

--- src/client/app/shared/models/filter-criteria.model.ts
+++ src/client/app/shared/models/filter-criteria.model.ts
@@ -1,41 +1,41 @@
-import {DateRangeType} from './date-range-type.model';
-import {Params} from '@angular/router';
+import { DateRangeType } from './date-range-type.model';
+import { Params } from '@angular/router';
 import * as _ from 'lodash';
 
 export class FilterCriteria {
-  fromDate:string; //iso
-  toDate:string; //iso
-  ascending:boolean = true;
-  rangeSelection:DateRangeType = DateRangeType.ALL_TIME;
-  playerIds:number[] = [];
-  factionIds:number[] = [];
-  agendaIds:number[] = [];
-  deckIds:number[] = [];
+  fromDate: string; //iso
+  toDate: string; //iso
+  ascending: boolean = true;
+  rangeSelection: DateRangeType = DateRangeType.ALL_TIME;
+  playerIds: number[] = [];
+  factionIds: number[] = [];
+  agendaIds: number[] = [];
+  deckIds: number[] = [];
 
-  static serialise(criteria:FilterCriteria):any {
+  static serialise(criteria: FilterCriteria): any {
     return _.cloneDeep(criteria);
   }
 
-  static deserialise(routeParams:Params):FilterCriteria {
+  static deserialise(routeParams: Params): FilterCriteria {
     const criteria = new FilterCriteria();
-    // TODO find out what's happened to RouteParams
-    // Object.assign(criteria, _.pick(routeParams.params, [
-    //   'fromDate', 'toDate', 'ascending', 'rangeSelection'
-    // ]));
-    // // param strings
-    // criteria.rangeSelection = +criteria.rangeSelection;
-    // if (routeParams.get('playerIds')) {
-    //   criteria.playerIds = Array.from(routeParams.get('playerIds')).map((id) => +id);
-    // }
-    // if (routeParams.get('factionIds')) {
-    //   criteria.factionIds = Array.from(routeParams.get('factionIds')).map((id) => +id);
-    // }
-    // if (routeParams.get('agendaIds')) {
-    //   criteria.agendaIds = Array.from(routeParams.get('agendaIds')).map((id) => +id);
-    // }
-    // if (routeParams.get('deckIds')) {
-    //   criteria.deckIds = Array.from(routeParams.get('deckIds')).map((id) => +id);
-    // }
+    // TODO come back and check this does what it's supposed to
+    Object.assign(criteria, _.pick(routeParams, [
+      'fromDate', 'toDate', 'ascending', 'rangeSelection'
+    ]));
+    // param strings
+    criteria.rangeSelection = +criteria.rangeSelection;
+    if (routeParams['playerIds']) {
+      criteria.playerIds = Array.from(routeParams['playerIds']).map((id) => +id);
+    }
+    if (routeParams['factionIds']) {
+      criteria.factionIds = Array.from(routeParams['factionIds']).map((id) => +id);
+    }
+    if (routeParams['agendaIds']) {
+      criteria.agendaIds = Array.from(routeParams['agendaIds']).map((id) => +id);
+    }
+    if (routeParams['deckIds']) {
+      criteria.deckIds = Array.from(routeParams['deckIds']).map((id) => +id);
+    }
     return criteria;
   }
 }
