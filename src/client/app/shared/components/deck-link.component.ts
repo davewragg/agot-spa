b732af9d4620f64ec348fb59f8a348dce7921738
Dave Wragg <david.wragg@gmail.com>
2017-01-01 14:08:40 +0000

    Fix routerLink syntax across the board (probably)

--- src/client/app/shared/components/deck-link.component.ts
+++ src/client/app/shared/components/deck-link.component.ts
@@ -2,12 +2,10 @@ import { Component, Input } from '@angular/core';
 // import { ROUTER_DIRECTIVES } from '@angular/router';
 import { Deck } from '../models/deck.model';
 
-// TODO ROUTER_DIRECTIVES replaced?
-
 @Component({
   moduleId: module.id,
   selector: 'agot-deck-link',
-  template: `<a [routerLink]="['/DeckDetails', {id: deck?.deckId}]">{{ deck?.title || deck?.fallbackTitle || 'Deck' }}</a>`,
+  template: `<a [routerLink]="['/decks', deck?.deckId]">{{ deck?.title || deck?.fallbackTitle || 'Deck' }}</a>`,
   // directives: [ROUTER_DIRECTIVES]
 })
 export class DeckLinkComponent {
