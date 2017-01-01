f478364e093bbdc95875abeffd6afef0bc054349
Dave Wragg <david.wragg@gmail.com>
2017-01-01 12:44:43 +0000

    Fix routerLink syntax

--- src/client/app/shared/components/player-link.component.ts
+++ src/client/app/shared/components/player-link.component.ts
@@ -4,7 +4,7 @@ import { Player } from '../models/player.model';
 @Component({
   moduleId: module.id,
   selector: 'agot-player-link',
-  template: `<a [routerLink]="['/PlayerDetails', {id: player?.playerId}]">{{ player?.name || player?.playerName || 'Player' }}</a>`,
+  template: `<a [routerLink]="['/players', player?.playerId]">{{ player?.name || player?.playerName || 'Player' }}</a>`,
 })
 export class PlayerLinkComponent {
   @Input()
