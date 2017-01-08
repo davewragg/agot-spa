4ca15e2f0430dd6e1e6a18c2a6dbdc20a09114ed
Dave Wragg <david.wragg@gmail.com>
2017-01-08 20:26:41 +0000

    Start reconstructing player stats

--- src/client/app/shared/models/player-insights.model.ts
+++ src/client/app/shared/models/player-insights.model.ts
@@ -1,42 +1,8 @@
 import { Faction } from './faction.model';
 import { Agenda } from './agenda.model';
 import { DeckClassStats } from './deck-class-stats.model';
-import { Deck } from './deck.model';
-
-interface DeckStats {
-  deck: Deck;
-  count: number;
-  winPercent?: number;
-}
-
-interface DeckResult {
-  deckId: number;
-  deck: Deck;
-  gamesWon: number;
-  gamesLost: number;
-  gamesDrawn: number;
-  totalGamesPlayed: number;
-  winPercentage: number;
-  decksPlayed: Deck[];
-}
 
 export class PlayerInsights {
-  gamesWon: number;
-  gamesLost: number;
-  gamesDrawn: number;
-  gamesPlayed: number;
-  winPercentage:  number;
-  lossPercentage:  number;
-  factionsNotPlayed: number[];
-  agendasNotPlayed: number[];
-  mostUsedDeck: DeckStats;
-  mostSuccessfulDeck: DeckStats;
-  leastSuccessfulDeck: DeckStats;
-  mostBeatenDeck: DeckStats;
-  mostLostToDeck: DeckStats;
-  deckResults: DeckResult[];
-
-  // legacy
   mostUsedDeckClass: DeckClassStats;
   mostSuccessfulDeckClass: DeckClassStats;
   leastSuccessfulDeckClass: DeckClassStats;
--- src/client/app/shared/models/player-stats.model.ts
+++ src/client/app/shared/models/player-stats.model.ts
@@ -2,8 +2,42 @@ import { Stats } from './stats.model';
 import { Game } from './game.model';
 import { StatsSet } from './stats-set.model';
 import { PlayerInsights } from './player-insights.model';
+import { Deck } from './deck.model';
+
+interface DeckStat {
+  deck: Deck;
+  count: number;
+  winPercent?: number;
+}
+
+interface DeckResult {
+  deckId: number;
+  deck: Deck;
+  gamesWon: number;
+  gamesLost: number;
+  gamesDrawn: number;
+  totalGamesPlayed: number;
+  winPercentage: number;
+  decksPlayed: Deck[];
+}
 
 export class PlayerStats {
+  gamesWon: number;
+  gamesLost: number;
+  gamesDrawn: number;
+  gamesPlayed: number;
+  winPercentage: number;
+  lossPercentage: number;
+  factionsNotPlayed: number[];
+  agendasNotPlayed: number[];
+  mostUsedDeck: DeckStat;
+  mostSuccessfulDeck: DeckStat;
+  leastSuccessfulDeck: DeckStat;
+  mostBeatenDeck: DeckStat;
+  mostLostToDeck: DeckStat;
+  deckResults: DeckResult[];
+
+  // legacy
   games: Game[] = [];
   overall: Stats = new Stats();
   as: StatsSet = new StatsSet();
