a1c872e9198c12805af0049a08590f1ec4a5876f
Dave Wragg <david.wragg@gmail.com>
2017-01-21 16:18:55 +0000

    Load current player into toolbar

--- src/client/app/app.component.ts
+++ src/client/app/app.component.ts
@@ -1,9 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
 import { Observable } from 'rxjs/Observable';
-import { Config } from './shared/config/env.config';
-import './operators';
 import { PlayerService } from './shared/services/player.service';
+import { Config } from './shared/config/env.config';
 import { Player } from './shared/models/player.model';
+import './operators';
 
 /**
  * This class represents the main application component.
@@ -12,6 +12,7 @@ import { Player } from './shared/models/player.model';
   moduleId: module.id,
   selector: 'agot-app',
   templateUrl: 'app.component.html',
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class AppComponent implements OnInit {
   currentPlayer: Observable<Player>;
--- src/client/app/shared/toolbar/toolbar.component.ts
+++ src/client/app/shared/toolbar/toolbar.component.ts
@@ -1,9 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, Input, ChangeDetectionStrategy } from '@angular/core';
+import { Player } from '../models/player.model';
 
 @Component({
   moduleId: module.id,
   selector: 'agot-toolbar',
   templateUrl: 'toolbar.component.html',
-  styleUrls: ['toolbar.component.css']
+  styleUrls: ['toolbar.component.css'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class ToolbarComponent { }
+export class ToolbarComponent {
+  @Input()
+  currentPlayer: Player;
+}
