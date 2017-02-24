c56a3aa707ecc4da8cd37449ba322bcabed2da5b
Dave Wragg <david.wragg@gmail.com>
2017-02-24 22:26:40 +0000

    Remove old cruft

--- src/client/app/shared/index.ts
+++ src/client/app/shared/index.ts
@@ -1,7 +1,6 @@
 /**
  * This barrel file provides the exports for the shared resources (services, components).
  */
-export * from './name-list/index';
 export * from './config/env.config';
 export * from './pipes/index';
 export * from './services/index';
