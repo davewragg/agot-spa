11fb91cc87d76849e6c0f720e70e66e0b724b9aa
Dave Wragg <david.wragg@gmail.com>
2017-03-16 21:03:58 +0000

    Don't need to look venue up here either

--- src/client/app/game/view-game.component.ts
+++ src/client/app/game/view-game.component.ts
@@ -1,8 +1,5 @@
 import { Component, Input, Output, EventEmitter, ChangeDetectionStrategy } from '@angular/core';
 import { Game } from '../shared/models/game.model';
-import { ReferenceDataService } from '../shared/services/reference-data.service';
-import { Observable } from 'rxjs/Observable';
-import { Venue } from '../shared/models/venue.model';
 
 @Component({
   moduleId: module.id,
@@ -19,19 +16,6 @@ export class ViewGameComponent {
   deleteGame: EventEmitter<Game> = new EventEmitter<Game>();
 
   deleting: boolean = false;
-
-  constructor(private _referenceDataService: ReferenceDataService) {
-  }
-
-  getVenue(venueId: number): Observable<Venue> {
-    return this._referenceDataService.getVenue(venueId);
-  }
-
-  getVenueName(venueId: number): Observable<string> {
-    return this.getVenue(venueId).map((venue: Venue) => {
-      return venue && venue.name;
-    });
-  }
 
   onEdit() {
     this.edit.emit(this.game);
