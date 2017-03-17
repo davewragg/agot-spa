0f6712266398d4f0d0f19d82270e5f33965dae86
Dave Wragg <david.wragg@gmail.com>
2017-03-17 20:43:15 +0000

    Convert home page to ngrx

--- src/client/app/home/home.component.ts
+++ src/client/app/home/home.component.ts
@@ -1,4 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, ChangeDetectionStrategy, OnInit } from '@angular/core';
+import { Store } from '@ngrx/store';
+import * as fromRoot from '../state-management/reducers/root';
+import * as rankingActions from '../state-management/actions/rankings';
+import * as gameActions from '../state-management/actions/game';
 import { FilterCriteria } from '../shared/models/filter-criteria.model';
 import { DateRangeType } from '../shared/models/date-range-type.model';
 
@@ -6,19 +10,22 @@ import { DateRangeType } from '../shared/models/date-range-type.model';
   moduleId: module.id,
   selector: 'agot-home',
   templateUrl: 'home.component.html',
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class HomeComponent {
-  gameFilterCriteria: FilterCriteria;
-  rankingFilterCriteria: FilterCriteria;
+export class HomeComponent implements OnInit {
+  constructor(private store: Store<fromRoot.State>) {
+  }
 
-  constructor() {
-    this.rankingFilterCriteria = Object.assign(new FilterCriteria(), {
+  ngOnInit() {
+    const rankingFilterCriteria = Object.assign(new FilterCriteria(), {
       ascending: true,
       rangeSelection: DateRangeType.ALL_TIME
     });
-    this.gameFilterCriteria = Object.assign(new FilterCriteria(), {
+    const gameFilterCriteria = Object.assign(new FilterCriteria(), {
       ascending: true,
       rangeSelection: DateRangeType.THIS_WEEK
     });
+    this.store.dispatch(new rankingActions.FilterAction(rankingFilterCriteria));
+    this.store.dispatch(new gameActions.FilterAction(gameFilterCriteria));
   }
 }
