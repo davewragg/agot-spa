1e323f0c6aaa452483df3301452958cb64342444
Dave Wragg <david.wragg@gmail.com>
2017-04-21 14:36:19 +0100

    Remove unnecessary filter

--- src/client/app/groups/groups.container.ts
+++ src/client/app/groups/groups.container.ts
@@ -1,11 +1,11 @@
-import { Component, OnDestroy, ChangeDetectionStrategy } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
-import { Store } from '@ngrx/store';
-import { Subscription } from 'rxjs/Subscription';
-import { isEmpty } from 'lodash';
-import * as fromRoot from '../state-management/reducers/root';
-import * as groupActions from '../state-management/actions/player-group.actions';
-import { FilterCriteria } from '../shared/models/filter-criteria.model';
+import { Component, /*OnDestroy, */ChangeDetectionStrategy } from '@angular/core';
+// import { ActivatedRoute } from '@angular/router';
+// import { Store } from '@ngrx/store';
+// import { Subscription } from 'rxjs/Subscription';
+// import { isEmpty } from 'lodash';
+// import * as fromRoot from '../state-management/reducers/root';
+// import * as groupActions from '../state-management/actions/player-group.actions';
+// import { FilterCriteria } from '../shared/models/filter-criteria.model';
 
 @Component({
   selector: 'agot-groups-page',
@@ -14,17 +14,17 @@ import { FilterCriteria } from '../shared/models/filter-criteria.model';
     <agot-groups></agot-groups>
   `
 })
-export class GroupsContainerComponent implements OnDestroy {
-  actionsSubscription: Subscription;
-
-  constructor(private store: Store<fromRoot.State>, route: ActivatedRoute) {
-    this.actionsSubscription = route.params
-      .map(params => isEmpty(params) ? new FilterCriteria() : FilterCriteria.deserialise(params))
-      .map(criteria => new groupActions.FilterAction(criteria))
-      .subscribe(store);
-  }
-
-  ngOnDestroy() {
-    this.actionsSubscription.unsubscribe();
-  }
+export class GroupsContainerComponent /*implements OnDestroy*/ {
+  // actionsSubscription: Subscription;
+  //
+  // constructor(private store: Store<fromRoot.State>, route: ActivatedRoute) {
+  //   this.actionsSubscription = route.params
+  //     .map(params => isEmpty(params) ? new FilterCriteria() : FilterCriteria.deserialise(params))
+  //     .map(criteria => new groupActions.FilterAction(criteria))
+  //     .subscribe(store);
+  // }
+  //
+  // ngOnDestroy() {
+  //   this.actionsSubscription.unsubscribe();
+  // }
 }
